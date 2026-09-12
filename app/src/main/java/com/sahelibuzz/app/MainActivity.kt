baf5a37497ddd38126f37f9ecceeee63f3b6ac77
antrikshpandey1710 <antrikshpandey304@gmail.com>
2026-09-12 09:32:35 +0530

    Update MainActivity.kt

--- app/src/main/java/com/sahelibuzz/app/MainActivity.kt
+++ app/src/main/java/com/sahelibuzz/app/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SaheliBuzzAuth()
+            SaheliBuzzApp()
         }
     }
 }
